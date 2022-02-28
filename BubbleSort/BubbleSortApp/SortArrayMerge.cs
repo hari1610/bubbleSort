3d015f6c17ad9f982fe26657700b58c1662fcaab
Hari Jalendran <hari.jalendran16@gmail.com>
2022-02-28 16:57:45 +0000

    added a mehod comment

--- BubbleSort/BubbleSortApp/SortArrayMerge.cs
+++ BubbleSort/BubbleSortApp/SortArrayMerge.cs
@@ -8,7 +8,7 @@ namespace BubbleSortApp
 {
     public class SortArrayMerge
     {
-        public static int[] MergeArray(int[] array1, int[] array2)
+        public static int[] MergeArray(int[] array1, int[] array2)//this method merges two arrays together
         {
             int[] output = new int[array1.Length + array2.Length];
             if (array1.Length == 0 && array2.Length == 0)
